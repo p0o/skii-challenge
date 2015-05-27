af4a627da323405538623d93c5e4404ec0a792ed
Pooria Attarzadeh <pooria_az@ymail.com>
2015-05-27 17:58:15 +0430

    last commit

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-var Skii = require('./skiii.js');
+var Skii = require('./skii.js');
 
 
 var skii = Skii();
 
-skii.load('testdata.txt');
+skii.load('map.txt');
 var result = skii.run();
 
 console.log(result.bestLength);
 console.log(result.bestDrop); 
 console.log('Finsihed in: ' + result.benchmark/1000 + ' seconds'); 
+
 
 
 //console.log(grid);
